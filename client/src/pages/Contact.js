49ff9323a86d4b9095e098b943ca277b0901346d
dylanocall <c21752815@mytudublin.ie>
2025-04-17 20:23:11 +0100

    Added images and screenshots folder for READme file

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,8 +6,9 @@ const Contact = () => {
     <div className="contact-container">
       <h2>About Us</h2>
       <p>
-        <strong>Student Housing Helper</strong> is a platform built to simplify the student housing experience in Ireland. 
-        Our goal is to connect students with compatible roommates and suitable housing based on lifestyle, preferences, and location.
+        <strong>Student Housing Helper</strong> is a platform built to simplify the student housing experience in Ireland. We have used our own first hand experience with these issues as
+        some of our team have spent months abroad attending college in different countries acroos Europe, eg Dylan attened college in Slovenia for 6 months.
+        Our goal is to connect students with compatible roommates and suitable housing based on lifestyle, preferences, affordability and location.
       </p>
       <p>
         We understand through our own personal expierence how hard it is to find reliable accommodation while managing college life. That’s why we’re developing smarter tools that make it easier for students to match, connect, and move in, without stress.
@@ -26,6 +27,7 @@ const Contact = () => {
       <h2>Our Mission</h2>
         <p>
         We’re on a mission to make student accommodation simple, trustworthy, and tailored to real student life.
+        Whether you are an Irish student or an international student coming to Ireland we can help.
         From matching with like-minded roommates to exploring verified listings, we’re building tools to help you live better during college.
         </p>
 
